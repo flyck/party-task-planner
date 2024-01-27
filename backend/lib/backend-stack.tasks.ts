fd06beae57975d8a506e216a8ac12412efeeabed
flyck <felix.brilej@googlemail.com>
2024-01-27 17:49:36 +0100

    api done

--- backend/lib/backend-stack.tasks.ts
+++ backend/lib/backend-stack.tasks.ts
@@ -1,4 +1,3 @@
-import * as cdk from 'aws-cdk-lib';
 import { Stack } from 'aws-cdk-lib';
 import * as appsync from 'aws-cdk-lib/aws-appsync';
 import { DynamoDbDataSource, GraphqlApi } from 'aws-cdk-lib/aws-appsync';
@@ -13,5 +12,85 @@ interface props {
 }
 
 export function createTaskResolvers({ api, participants, parties, stack, tasks }: props) {
-  return
+  // Make sure we only allow the creation of tasks for existing parties
+  // NICETOHAVE check existance of participant before creation or update if specified
+  const CreateTaskF1 = new appsync.AppsyncFunction(stack, "createTaskF1", {
+    api,
+    name: "CreateTaskGet",
+    dataSource: parties,
+    runtime: appsync.FunctionRuntime.JS_1_0_0,
+    code: appsync.Code.fromAsset(join(__dirname, "./resolvers/getRelatedParty.js")),
+  });
+
+  const CreateTaskF2 = new appsync.AppsyncFunction(stack, "createTaskF2", {
+    api,
+    name: "CreateTaskPut",
+    dataSource: tasks,
+    runtime: appsync.FunctionRuntime.JS_1_0_0,
+    code: appsync.Code.fromAsset(join(__dirname, "./resolvers/createTask.js"))
+  });
+  new appsync.Resolver(stack, "CreateTaskPipeline", {
+    api,
+    typeName: 'Mutation',
+    fieldName: 'createTask',
+    pipelineConfig: [CreateTaskF1, CreateTaskF2],
+    requestMappingTemplate: appsync.MappingTemplate.fromString('{}'),
+    responseMappingTemplate: appsync.MappingTemplate.fromString('$util.toJson($ctx.result)'),
+  });
+
+  // Make the update respect existing data. First resolver is a javascript resolver as well since
+  // javascript resolvers and templates cannot be mixed within the same pipeline
+  // TODO switch to updateItem https://dev.to/aws/20-days-of-dynamodb-day-4-conditional-updateitem-dac
+  const UpdateTaskF1 = new appsync.AppsyncFunction(stack, "updateTaskF1", {
+    api,
+    name: "UpdateTaskGet",
+    dataSource: tasks,
+    runtime: appsync.FunctionRuntime.JS_1_0_0,
+    code: appsync.Code.fromAsset(join(__dirname, "./resolvers/getTask.js")),
+  });
+
+  const UpdateTaskF2 = new appsync.AppsyncFunction(stack, "updateTaskF2", {
+    api,
+    name: "UpdateTaskPut",
+    dataSource: tasks,
+    runtime: appsync.FunctionRuntime.JS_1_0_0,
+    code: appsync.Code.fromAsset(join(__dirname, "./resolvers/updateParticipant.js"))
+  });
+
+  new appsync.Resolver(stack, "UpdateTaskPipeline", {
+    api,
+    typeName: 'Mutation',
+    fieldName: 'updateTask',
+    pipelineConfig: [UpdateTaskF1, UpdateTaskF2],
+    requestMappingTemplate: appsync.MappingTemplate.fromString('{}'),
+    responseMappingTemplate: appsync.MappingTemplate.fromString('$util.toJson($ctx.result)'),
+  });
+
+  tasks.createResolver("getTask", {
+    typeName: "Query",
+    fieldName: "getTask",
+    runtime: appsync.FunctionRuntime.JS_1_0_0,
+    code: appsync.Code.fromAsset(join(__dirname, "./resolvers/getTask.js"))
+  });
+
+  tasks.createResolver("getTasks", {
+    typeName: "Query",
+    fieldName: "getTasks",
+    runtime: appsync.FunctionRuntime.JS_1_0_0,
+    code: appsync.Code.fromAsset(join(__dirname, "./resolvers/getTasks.js"))
+  });
+
+  participants.createResolver("getPartyTasks", {
+    typeName: "Party",
+    fieldName: "tasks",
+    runtime: appsync.FunctionRuntime.JS_1_0_0,
+    code: appsync.Code.fromAsset(join(__dirname, "./resolvers/getPartyParticipants.js"))
+  });
+
+  tasks.createResolver("deleteTask", {
+    typeName: "Mutation",
+    fieldName: "deleteTask",
+    runtime: appsync.FunctionRuntime.JS_1_0_0,
+    code: appsync.Code.fromAsset(join(__dirname, "./resolvers/deleteTask.js"))
+  });
 }
